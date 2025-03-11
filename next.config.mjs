a89e2a19b0c942d77d69428a2b05c6e7bdd3a081
amankumar2k24 <aman.kumar@webitoinfotech.com>
2025-03-12 01:21:39 +0530

    changes in nextConfig file with aws s3 bucket

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        remotePatterns: {
+            protocol: "https",
+            hostname: "pm-s3-buckets.s3.ap-south-1.amazonaws.com",
+            port: "",
+            pathname: "/**",
+        }
+    }
 };
 
 export default nextConfig;
